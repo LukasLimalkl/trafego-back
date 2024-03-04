27ae12de8d5fe562853fa12d88596367903a7afa
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-04 12:56:10 -0300

    feat: adding gorm

--- cmd/trafego-back/main.go
+++ cmd/trafego-back/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"github.com/LukasLimalkl/trafego-back/internal/router"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	router.Initialize()
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
+
 }
